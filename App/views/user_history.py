425fe4085efc1f98d7dfba3e1dfdb46d7d373617
Zurun-Rikuthin <y.verinrii@gmail.com>
2024-04-18 08:44:20 +0000

    made game history viewable (incomplete) and added minor styling

--- App/views/user_history.py
+++ App/views/user_history.py
@@ -2,30 +2,34 @@ from flask import Blueprint, render_template, jsonify, request, send_from_direct
 
 from App.database import db
 from App.models import Game, User, UserGuess
-from datetime import datetime
+from datetime import date
 
 user_history_views = Blueprint('user_history_views', __name__, template_folder='../templates')
 
-@user_history_views.route("/user_history/<user_id>", defaults={"game_id" : None}, methods=["GET"])
-@user_history_views.route("/user_history/<user_id>/<game_id>", methods=["GET"])
-def user_history(user_id, game_id):
+@user_history_views.route("/user_history/<user_id>", methods=["GET"]) #defaults={"game_id" : None}, methods=["GET"])
+#@user_history_views.route("/user_history/<user_id>/<game_id>", methods=["GET"])
+def user_history(user_id):
     user = User.query.filter_by(id=user_id).first()
     
     if not user:
         return render_template("404.html", error=f"user with ID <{user_id}> does not exist")
     
-    game = Game.query.filter_by(id=user_id).first()
+    guesses = UserGuess.query.filter_by(user_id=user.id).all()
+    game_ids = set()
 
-    user_games = None
-    user_guesses = None
-    
-    user_games = UserGuess.query.filter_by(user_id=user.id).first()
+    for i in guesses:
+        if i.game_id not in game_ids:
+            game_ids.add(i.game_id)
 
-    # Cannot select a game without first selecting a user
-    if game:
-        user_guesses = UserGuess.query.filter_by(user_id=user.id, game_id=game.id).first()
+    games = Game.query.filter(Game.id.in_(game_ids)).all()
+    user_games = []
+    for i in games:
+        user_games.append({
+            "date" : i.creation_date,
+            "answer" : "[REDACTED]" if date.today == i.creation_date else i.answer,
+            "guesses" : [j for j in guesses if j.game_id == i.id]
+        })
 
     return render_template("user_history.html",
                         user=user,
-                        user_games=user_games,
-                        user_guesses=user_guesses)
+                        user_games=user_games)
