4e12a12a9693e9a3626cd544015dca727a5e9ab4
Zurun-Rikuthin <y.verinrii@gmail.com>
2024-04-20 14:27:17 +0000

    continued expanding the how to play page

--- App/views/howtoplay.py
+++ App/views/howtoplay.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for, session
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 
 howtoplay_views = Blueprint('howtoplay_views', __name__, template_folder='../templates')
@@ -8,5 +8,9 @@ Page Routes
 '''   
 @howtoplay_views.route('/howtoplay', methods=['GET'])
 def howtoplay_page():
+    if 'explain_image_hover_expand' not in session:
+        flash("Hover over an image to expand it.")
+        session['explain_image_hover_expand'] = True
+        
     return render_template('howtoplay.html')
     
